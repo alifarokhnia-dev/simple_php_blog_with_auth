aebccc66472ff46255455ccb151db5658ea71c71
AliFarokhnia <alifarokhnia.dev@proton.me>
2025-09-18 19:59:03 +0330

    signup feature added

--- add_user.php
+++ add_user.php
@@ -3,24 +3,22 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $password = password_hash($password, PASSWORD_DEFAULT);
 
     $conn = new mysqli("localhost", "root", "", "userdb");
     # check to see if email already exists;
-    $sql = "select 1 from users where email = '?'";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    $sql = "select 1 from users where email = '$email'";
+    $result = $conn->query($sql);
+    
     session_start();
-    echo $result['email'];
-    if (! $result === []){
+    if (mysqli_num_rows($result) > 0){
         $_SESSION['message'] = 'email already exists.';
         $_SESSION['message_class'] = 'error';
         header('Location: signup.php');
     } else {
         $sql = "INSERT INTO users(fullname, email, password) VALUES(?, ?, ?)";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sss", $name, $email, password_hash($password));
+        $stmt->bind_param("sss", $name, $email, $password);
         $stmt->execute();
         $_SESSION['message'] = "account created successfully!";
         $_SESSION['message_class'] = 'success';
--- signup.php
+++ signup.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .error{color: red;}
+        .success{color:green;}
+        .warn{color: yellow;}
+    </style>
 </head>
 <body>
     <?php
